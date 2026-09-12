067f5a7cdf5db970f5f35d7256d73eee184c43d4
James Zhu <hongyizhu2007@outlook.com>
2026-09-12 16:11:31 -0400

    solution

--- Lab_01/03_integer_operations.cpp
+++ Lab_01/03_integer_operations.cpp
@@ -1,19 +1,28 @@
-// Program to Add/Subtract/Multiply/Divide Two Integers.
+//C++ Program to Add/Subtract/Multiply/Divide Two Integers.
 
-#include <iostream>
+#include <iostream> 
 
-using namespace std;
+int main ();
+int main ()
 
-int main()
 {
-    int num1, num2;
-    cout << "Enter two integers: ";
-    cin >> num1 >> num2;
-    cout << "Sum: " << num1 + num2 << endl;
-    cout << "Difference: " << num1 - num2 << endl;
-    cout << "Product: " << num1 * num2 << endl;
-    cout << "Quotient: " << num1 / num2 << endl;
-    cout << "Remainder: " << num1 % num2 << endl;
-    system("pause");
+    int num1,num2; 
+    std::cout <<"enter two integers: "; 
+    std::cin >> num1 >> num2;
+
+
+    std::cout <<"Sum: " << num1+num2 << std::endl; 
+    std::cout <<"Difference: " << num1-num2 << std::endl; 
+    std::cout <<"Product: " << num1*num2 << std::endl; 
+    if (num2 != 0)
+    {
+        std::cout <<"Quotient: " << num1/num2 << std::endl; 
+        std::cout <<"Remainder: " << num1%num2 << std::endl; 
+    }
+    else
+    {
+        std::cout <<"Quotient and remainder are undefined when dividing by zero." << std::endl;
+    }
+    
     return 0;
 }
