441eb490d99948c707299b289e2ddab33a96c267
James Zhu <hongyizhu2007@outlook.com>
2026-09-12 03:23:19 -0400

    solution

--- Lab_01/02_take_user_input.cpp
+++ Lab_01/02_take_user_input.cpp
@@ -1,16 +1,14 @@
-// Program to print an integer entered by the user.
-// cin -> console input.
+//C++ Program to print an integer entered by the user. 
 
-#include <iostream>
+#include <iostream> 
 
-using namespace std;
+int main(); 
+int main(){ 
+    int number; 
+    std::cout << "Enter your number:"; 
+    std::cin >> number; 
+    std::cout << "Your number is: " << number; 
+    std::cout << std::endl; 
 
-int main()
-{
-    int number;
-    cout << "Enter an integer: ";
-    cin >> number;
-    cout << "You entered: " << number << endl;
-    system("pause");
     return 0;
 }
